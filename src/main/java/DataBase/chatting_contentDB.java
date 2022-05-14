9494a40e03d0eaacfeda75753e1d638491bc13d1
@pan-changxing <2369199640@qq.com>
2022-05-14 21:33:29 +0800

    完成了聊天内容数据库和群组数据库的建立和相应数据操作的实现

---
+++ src/main/java/DataBase/chatting_contentDB.java
@@ -0,0 +1,64 @@
+package DataBase;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *用来进行聊天内容数据库操作
+ */
+
+public class chatting_contentDB {
+
+    private Connection conn;
+    private Statement stmt;
+
+    public chatting_contentDB() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url="jdbc:mysql://101.43.157.247:3306/IM";
+            this.conn=DriverManager.getConnection(url, "root", "55611224");
+
+             if(!this.conn.isClosed())
+             System.out.println("Database is connected!");
+
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    //增刪改
+    public int update(String sql) {
+        System.out.println("sql:"+sql);
+        try {
+            this.stmt=this.conn.createStatement();
+            return this.stmt.executeUpdate(sql);		//返回 影响 了几行
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+
+    //查询方法
+    public ResultSet query(String sql) {
+        //System.out.println("sql:"+sql);
+        try {
+            this.stmt=this.conn.createStatement();
+            return this.stmt.executeQuery(sql);
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
+
